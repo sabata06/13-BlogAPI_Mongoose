8c49101986ede26e648cde26e822efc2a48d34b4
Your Name <m.karaarslan06@outlook.com>
2024-02-29 23:07:17 +0300

    sync file updated

--- src/sync.js
+++ src/sync.js
@@ -1,11 +1,10 @@
-"use strict"
+"use strict";
 
-const User = require('./models/userModel')
-const { BlogCategory, BlogPost } = require('./models/blogModel')
+const User = require("./models/userModel");
+const { BlogCategory, BlogPost } = require("./models/blogModel");
 
 module.exports = async () => {
-
-    /* User */
+  /* User *
 
     // Get first user:
     const user = await User.findOne()
@@ -20,7 +19,7 @@ module.exports = async () => {
         }).catch(err => console.log(err))
     }
 
-    /* BlogCategory */
+    /* BlogCategory *
 
     // Get first blogCategory:
     const blogCategory = await BlogCategory.findOne()
@@ -35,7 +34,39 @@ module.exports = async () => {
         }).catch(err => console.log(err))
     }
 
-    // End:
-    console.log('* Synchronized *')
-    /* Finished */
-}
+    /* Exampla Data */
+  // Deleted All Records:
+  await User.deleteMany().then(() => console.log(" - User Deleted All"));
+  await BlogCategory.deleteMany().then(() =>
+    console.log(" - BlogCategory Deleted All")
+  );
+  await BlogPost.deleteMany().then(() =>
+    console.log(" - BlogPost Deleted All")
+  );
+
+  // Example User:
+  const user = await User.create({
+    email: "test@test.com",
+    password: "12345678",
+    firstName: "Test",
+    lastName: "Test",
+  });
+  // Example Category:
+  const blogCategory = await BlogCategory.create({
+    name: "Test Category",
+  });
+  // Example Posts:
+  for (let key in [...Array(200)]) {
+    await BlogPost.create({
+      userId: user._id,
+      blogCategoryId: blogCategory._id,
+      title: `test ${key} title`,
+      content: `test ${key} content`,
+      published: Boolean(key % 2),
+    });
+  }
+
+  // End:
+  console.log("* Synchronized *");
+  /* Finished */
+};
