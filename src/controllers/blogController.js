23661c3b5229bac04dffbf29544325ece9d39fd9
Your Name <m.karaarslan06@outlook.com>
2024-03-01 23:27:29 +0300

    blog controller updated

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -17,7 +17,8 @@ const { BlogCategory, BlogPost } = require("../models/blogModel");
 // ------------------------------------------
 module.exports.BlogCategory = {
   list: async (req, res) => {
-    const data = await BlogCategory.find();
+    // const data = await BlogCategory.find()
+    const data = await req.getModelList(BlogCategory);
 
     res.status(200).send({
       error: false,
@@ -75,20 +76,48 @@ module.exports.BlogCategory = {
 // ------------------------------------------
 module.exports.BlogPost = {
   list: async (req, res) => {
-    const search = req.query?.search || {};
-    // console.log(search);
+    /*
+        // Searching & Sorting & Pagination:
 
-    for (let key in search)
-      search[key] = { $regex: search[key], $options: "i" };
+        // SEARCHING: URL?search[key1]=value1&search[key2]=value2
+        const search = req.query?.search || {}
+        // console.log(search)
+        // https://www.mongodb.com/docs/manual/reference/operator/query/regex/
+        for (let key in search) search[key] = { $regex: search[key], $options: 'i' } // i: case Insensitive
+        // console.log(search)
 
-    const data = await BlogPost.find(search);
-    console.log(search);
+        // Cancelled -> SORTING: URL?sort[key1]=1&sort[key2]=-1 (1:ASC, -1:DESC)
+        // mongoose=^8.0 -> SORTING: URL?sort[key1]=asc&sort[key2]=desc (asc: A->Z - desc: Z->A)
+        const sort = req.query?.sort || {}
+        // console.log(sort)
 
-    // const data = await BlogPost.find().populate("blogCategoryId"); // get Primary Data
+        // PAGINATION: URL?page=1&limit=10
+        // const limit = req.query?.limit || 20
+        // let limit = req.query?.limit || (process.env?.PAGE_SIZE || 20)
+        // limit = Number(limit)
+        let limit = Number(req.query?.limit)
+        limit = limit > 0 ? limit : Number(process.env?.PAGE_SIZE || 20)
+        // console.log('limit', typeof limit, limit)
+
+        let page = Number(req.query?.page)
+        page = (page > 0 ? page : 1) - 1 // Backend'de sayfaNo her zaman -1'dir.
+        // console.log('page', typeof page, page)
+
+        let skip = Number(req.query?.skip) // İstenirse url'de ?skip=10 gibi değer gönderilebilir.
+        skip = skip > 0 ? skip : (page * limit)
+        // console.log('skip', typeof skip, skip)
+
+        // RUN:
+        // const data = await BlogPost.find().populate('blogCategoryId') // get Primary Data
+        const data = await BlogPost.find(search).sort(sort).skip(skip).limit(limit).populate('blogCategoryId')
+        */
+
+    const data = await req.getModelList(BlogPost, "blogCategoryId");
 
     res.status(200).send({
       error: false,
       count: data.length,
+      details: await req.getModelListDetails(BlogPost),
       result: data,
     });
   },
